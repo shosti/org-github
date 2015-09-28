50d8727b9bf0b98446f3cabd5bb064ca6e21fe38
Emanuel Evans <emanuel.evans@gmail.com>
2015-09-27 18:21:30 -0700

    Add boilerplate

---
+++ org-github.el
@@ -0,0 +1,39 @@
+;;; org-github --- Organize your Github issues with org-mode -*- lexical-binding: t -*-
+
+;; Copyright © 2015 Emanuel Evans
+
+;; Author: Emanuel Evans <mail@emanuel.industries>
+;; Maintainer: Emanuel Evans <mail@emanuel.industries>
+;; URL: http://github.com/shosti/org-github
+;; Version: 0.0.1
+;; Created: 27 Sep 2015
+;; Package-Requires: ((emacs "24") (seq "1.9"))
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Organize and edit your Github issues with org-mode.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(provide 'org-github)
+
+;;; org-github.el ends here
